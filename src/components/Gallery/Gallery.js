1aeacd9460bd0514ed87dc3f7a6942fe9a2c4cf1
Iuri <nicoguloso2@gmail.com>
2024-01-19 13:38:17 +0100

    updated gallery.js and created services folder with logementService.js to prepare for future backend integration

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Card from "../Card/Card";
-import locationData from "../../assets/data/logements.json";
 import styles from "./gallery.module.scss";
 
+// Importez votre module de service (services/logementService.js) ici
+import { getAllHouses } from "../../services/logementService";
+
 const Gallery = () => {
-    return (
-        <div className={styles.container}>
-            {locationData.map((location) => (
-                <article key={location.id}>
-                    <Link to={`/logement/${location.id}`}>
-                        <Card image={location.cover} title={location.title} />
-                    </Link>
-                </article>
-            ))}
-        </div>
-    );
+  const [houses, setHouses] = useState([]);
+
+  useEffect(() => {
+    // Utilisez une fonction pour obtenir les données des logements
+    async function fetchData() {
+      try {
+        // Ici, vous pouvez remplacer cet appel fictif par l'appel à votre backend lorsque vous l'aurez
+        const data = await getAllHouses(); // Utilisez la fonction du service
+        setHouses(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // Appelez fetchData au moment du chargement du composant
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {houses.map((location) => (
+        <article key={location.id}>
+          <Link to={`/logement/${location.id}`}>
+            <Card image={location.cover} title={location.title} />
+          </Link>
+        </article>
+      ))}
+    </div>
+  );
 };
 
 export default Gallery;
